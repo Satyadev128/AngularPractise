ec7193cd230ce86f3a42cdd656b528be1c11a030
Satyadev128 <satyadev.dangeti@platform3solutions.com>
2017-07-13 12:34:38 -0500

    Removed Tree Component

--- ArcProHub/src/app/app.module.ts
+++ ArcProHub/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -6,7 +7,6 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-
     HeaderComponent
   ],
   imports: [
