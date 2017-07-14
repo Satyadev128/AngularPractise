afaae8f8f66058422d9754236e282db2366d81a4
Satyadev128 <satyadev.dangeti@platform3solutions.com>
2017-07-14 16:07:23 -0500

    Adding Signin, Purchase buttons and logo

--- ArcProHub/src/app/app.module.ts
+++ ArcProHub/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyContentComponent } from './header/body-content/body-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyContentComponent
   ],
   imports: [
     BrowserModule
